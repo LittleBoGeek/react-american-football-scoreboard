8eed09cbf651cdfbda1a3a67a22c105b7cc52fda
LittleBoGeek <NadiaChilds@outlook.com>
2020-04-08 22:07:04 -0400

    stretch completed, can manipulate quarters

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
+
 const BottomRow = () => {
+  const [gameQuarter, setGameQuarter ] = useState(1);
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,10 +20,13 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{gameQuarter}</div>
+        <button onClick={() => setGameQuarter(gameQuarter + 1)} className="awayButtons__touchdown"> +1/4 </button>
+{gameQuarter >= 5 ? setGameQuarter(0) : console.log("keep going")}
       </div>
     </div>
   );
 };
 
 export default BottomRow;
+
